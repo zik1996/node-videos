75b5f43a95c1bb30a8bdba3d2aec3b1e23a97cf2
zik1996 <zikrullahha2212@gmail.com>
2024-08-07 14:57:45 +0530

    this is next version

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cors());
 
-// This is home page
+// This is Home route 
 app.get("/", (req, res)=>{
     res.send("<h1>Welcome to Node JS</h1>")
 })
 
-//this is admin routes
+//this is admin rote
 app.get("/admin", (req, res)=>{
     mongoClient.connect(constr).then((clientObject)=>{
         var database = clientObject.db("reactdb");
